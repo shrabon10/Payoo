273206593566501995f33f4b22f13f2d417d6128
shrabon10 <omarfaruksrabon@gmail.com>
2026-02-18 14:22:13 +0600

    Successfully Done Log-In Page

---
+++ script/login.js
@@ -0,0 +1,20 @@
+document.getElementById("login-btn").addEventListener('click', function(){
+    const numberInput = document.getElementById("input-number");
+    const contuctNumber = numberInput.value;
+    console.log(contuctNumber);
+
+    const inputpin = document.getElementById("input-pin")
+    const pin = inputpin.value;
+    console.log(pin);
+
+    if(contuctNumber == "01749761853" && pin == "1234"){
+        // alert("Successfully Log");
+        window.location.assign("/home.html")
+    }else{
+        alert("please Try Again")
+        return;
+    }
+    
+    
+    
+})
